cdaa573202065c121643b1fca43d3cddf78fd48a
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2024-12-08 12:08:20 -0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  trailingSlash: true, // Optional, adds a trailing slash to URLs
+};
+
+export default nextConfig;
